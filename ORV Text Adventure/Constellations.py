5ef04c5628493d78feb2d4736a0d70ab2cba7ea3
yodaddycoder <142638508+yodaddycoder@users.noreply.github.com>
2025-06-22 17:17:49 -0500

    made constellations page and put all constellation stuff there

---
+++ ORV Text Adventure/Constellations.py
@@ -0,0 +1,62 @@
+class Constellation:
+    def __init__(self, name, tier, stigma):
+        self.name = name
+        self.tier = tier
+        self.stigma = stigma
+
+# Constellations and their stigmas
+def steelLikeDefense():
+    pass 
+masterOfSteel = Constellation("Master of Steel", "Narrative Grade", steelLikeDefense)
+
+def songOfTheWater():
+    pass
+navalGeneral = Constellation("Naval General", "Historical Grade", songOfTheWater)
+
+def abyssOfDarkness():
+    pass
+darkVoidedBird = Constellation("Dark Voided Bird", "Narrative Grade", abyssOfDarkness)
+
+def timeOfVerdict():
+    pass
+juryOfWater = Constellation("Jury of Water", "Narrative Grade", timeOfVerdict)
+
+def scheming():
+    pass
+deviousSchemer = Constellation("Devious Schemer", "■■■■■■■", scheming)
+
+def shineBrightLikeADiamond():
+    pass
+queenOfTheElevenStars = Constellation("Ablazing Sun", "Narrative Grade", shineBrightLikeADiamond)
+
+def lesserBeing():
+    pass
+duskEffigy = Constellation("Dusk Effigy", "■■■■■■■", lesserBeing)
+
+def pinpoint():
+    pass
+goldenStickAndString = Constellation("Golden Stick and String", "Narrative Grade", pinpoint)
+
+def pewPewPew():
+    pass
+offenseMaster = Constellation("Offense Master", "Historical Grade", pewPewPew)
+
+def bloodSwipe():
+    pass
+redBloodManipulator = Constellation("Red Blood Manipulator", "Historical Grade", bloodSwipe)
+
+def grandPitch():
+    pass
+godOfTheOpera = Constellation("God of The Opera", "Historical Grade", grandPitch)
+
+def crucifix():
+    pass
+theGrandPriest = Constellation("The Grand Priest", "Historical Grade", crucifix)
+
+def impendingThunder():
+    pass
+kingOfTheAtmosphericallyChargedChair = Constellation("King of The Atmospherically Charged Chair", "Myth Grade", impendingThunder)
+
+# Chooses Constellation
+def randomConstellation():
+    pass
