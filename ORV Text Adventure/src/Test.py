71dffb68ea587093b3ef6b83f1a7c0a1c7907c76
cycIes <alexisvu77@gmail.com>
2025-07-02 02:50:13 -0500

    add more item details

--- ORV Text Adventure/src/Test.py
+++ ORV Text Adventure/src/Test.py
@@ -1,13 +1,19 @@
 from character import Character as c, Item as item
 
-# Character creation test
+### Character creation test
 player = c.MainCharacter("Boo")
 player.displayAttributesWindow()
-# Inventory tests
+### Inventory tests
 def basicAbility(): print(f"Used a basic ability!")
 something = item.Item("something", False, item.Grade.A, basicAbility)
-somethingElse = item.Item("somethingElse", True, item.Grade.A, basicAbility)
-aThing = item.Item("aThing", True, item.Grade.A, basicAbility)
+something.addDescription("This item was created for some reason idk.")
+something.addAbilityDescription("Wow this item does something.")
+
+somethingElse = item.Item("somethingElse", True, item.Grade.D, basicAbility)
+somethingElse.addDescription("This item was created for some reason idk.")
+somethingElse.addAbilityDescription("Wow this item does something. Oh btw it can only be used once.")
+
+aThing = item.Item("aThing", True, item.Grade.Constellation, basicAbility)
 
 player.inventory.displayInventory()
 player.inventory.addToInventory(something, 1)
@@ -20,13 +26,16 @@ player.inventory.removeFromInventory(somethingElse, -1)
 player.inventory.removeFromInventory(aThing, 1)
 player.inventory.addToInventory(something, 1)
 player.inventory.displayInventory()
-# Equipped item tests
-player.useEquipped()
-player.inventory.accessInventory() # Equip somethingElse
-player.inventory.showEquipped()
-player.useEquipped()
-player.inventory.displayInventory()
-player.inventory.accessInventory() # Equip something
-player.inventory.showEquipped()
-player.useEquipped()
-player.inventory.displayInventory()
+### Equipped item tests
+# player.useEquipped()
+# player.inventory.accessInventory() # Equip somethingElse
+# player.inventory.showEquipped()
+# player.useEquipped()
+# player.inventory.displayInventory()
+# player.inventory.accessInventory() # Equip something
+# player.inventory.showEquipped()
+# player.useEquipped()
+# player.inventory.displayInventory()
+### Item info tests
+something.displayDetails()
+somethingElse.displayDetails()
--- ORV Text Adventure/src/character/Item.py
+++ ORV Text Adventure/src/character/Item.py
@@ -1,7 +1,10 @@
 from enum import Enum
+import Text as text
 
 class Grade(Enum):
-    Constellation = "Constellation"
+    Constellation = "Constellation Artifact"
+    SSS = "SSS"
+    SS = "SS"
     S = "S"
     A = "A"
     B = "B"
@@ -14,9 +17,26 @@ class Item:
         self.consumable = consumable
         self.grade = grade
         self.ability = ability
+        self.description = "No description provided."
+        self.abilityDescription = "No ability information provided."
     
     def isConsumable(self):
         return self.consumable
     
     def activateAbility(self):
         self.ability()
+    
+    def addDescription(self, description):
+        self.description = description
+
+    def addAbilityDescription(self, description):
+        self.abilityDescription = description
+    
+    def displayDetails(self):
+        text.displayLine()
+        print("[Item Information]")
+        print(f"Name: {self.name}")
+        print(f"Grade: {self.grade.value}")
+        print(f"Description: {self.description}")
+        print(self.abilityDescription)
+        text.displayLine()
