c8785183fa877875beb2d21868a4763e94553305
yodaddycoder <142638508+yodaddycoder@users.noreply.github.com>
2025-07-14 15:23:28 -0500

    added a list of random names

--- ORVTextAdventure/src/character/RandomPerson.py
+++ ORVTextAdventure/src/character/RandomPerson.py
@@ -2,16 +2,23 @@ import random as rand
 from character.Character import Character
 from character.Inventory import Inventory
 
-LiamRyder = Character("Liam Ryder", 26)
+firstNames = []
+lastNames = []
 
-OliviaBaker = Character("Olivia Baker", 37)
 
-SamDavis = Character("Sam Davis", 15)
+#LiamRyder = Character("Liam Ryder", 26)
+
+#OliviaBaker = Character("Olivia Baker", 37)
+
+#SamDavis = Character("Sam Davis", 15)
 
 randomPersonList = [LiamRyder, OliviaBaker, SamDavis]
 
 def randomPersonRandomizer():
     global randomPersonList
     rand.shuffle(randomPersonList)
+
+def createRandomPerson():
+    pass
     
 # ideas: could randomize name combination and age
